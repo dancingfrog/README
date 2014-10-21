c1b19f51a82d08b2c7a4e151fb9c6ed1b0d755e4
Revlin John <stylogicalmaps@gmail.com>
2014-10-21 14:28:03 -0700

    add route: /dev

--- uni-sol.pl
+++ uni-sol.pl
@@ -1,6 +1,13 @@
 # This file defines routes for uni-sol (https://github.com/uni-sol/uni-sol)
 use Mojo::Log;
 
+get '/dev' => sub {
+	my $self = shift;
+	#getReadme($self, 'revlin/README.md');
+	getSVGReadme($self, 'revlin/README.md');
+	
+};
+
 get '/revlin' => sub {
 	my $self = shift;
 	#getReadme($self, 'revlin/README.md');
